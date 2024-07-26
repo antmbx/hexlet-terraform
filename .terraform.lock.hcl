250012f9d8b32467574f3efbffcb5b37c1587835
Anton Dmitriev <antmbx@gmail.com>
2024-07-27 01:09:42 +0300

    terraform

---
+++ .terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version = "0.124.0"
+  hashes = [
+    "h1:i+Q/CZ6Rue0W/kFT8fBXJE/u8t5Jl/5oczPHmYVWcaU=",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+// Terraform должен знать ключ, для выполнения команд по API
+
+// Определение переменной, которую нужно будет задать
+variable "yc_token" {}
+
+provider "yandex" {
+  zone = "ru-central1-a"
+  token = var.yc_token
+}
